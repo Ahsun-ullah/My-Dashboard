7e8e6e72f68d015ff227325f7758848cbbf17b51
ahsun rahim <ahsunrahim@gmail.com>
2023-06-16 17:58:16 +0600

    pdate the next config problem

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    
-  }
+// /** @type {import('next').NextConfig} */
+//   const nextConfig = {
+//     reactStrictMode: true,
 
-  module.exports = nextConfig
+//   }
+
+//   module.exports = nextConfig
+
+module.exports = {
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+      // Add more route mappings here if needed
+    };
+  },
+  exportTrailingSlash: true, // Enable trailing slashes in exported URLs
+};
