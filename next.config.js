a18c8ab7a9be0853605b9c335cfdb7868fcf814a
ahsun rahim <ahsunrahim@gmail.com>
2023-06-16 18:00:41 +0600

    pdate the next config problem

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-// /** @type {import('next').NextConfig} */
-//   const nextConfig = {
-//     reactStrictMode: true,
-
-//   }
-
-//   module.exports = nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
 module.exports = {
+  nextConfig,
   exportPathMap: function () {
     return {
       "/": { page: "/" },
